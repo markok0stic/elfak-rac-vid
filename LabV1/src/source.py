de99642299730ec2a4da9ffa46f27a85197ce281
markok0stic <94057185+markok0stic@users.noreply.github.com>
2023-03-07 19:13:38 +0100

    LabV1

--- LabV1/src/source.py
+++ LabV1/src/source.py
@@ -1,16 +1,15 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def crniKvadrati(matrix, x, y):
+def anullPixels(matrix, x, y):
     for i in range(-2, 2):
         for j in range(-2, 2):
             matrix[x + i, y + j] = 0
 
 
 # Load gray image
-img = cv2.imread('input.png', 0)
+img = cv.imread('../assets/input.png', 0)
 
 # Perform 2D Fourier Transform
 f = np.fft.fft2(img)
@@ -23,32 +22,31 @@ magnitude_spectrum = 20 * np.log(np.abs(fshift))
 
 # Display the magnitude spectrum
 plt.imshow(magnitude_spectrum, cmap='gray')
-plt.title('Magnituda spektra pre uklanjanja šuma')
+plt.title('Magnitude spectrum before filtering')
 plt.savefig('fft_mag.png')
 plt.show()
 
 
-# ovo rucno gledam samo gde su beli pixeli (4 ih ima)
-# ovo su koordinate tih pixela
-y1, x1 = 236, 156
-y2, x2 = 276, 156
-y3, x3 = 236, 356
-y4, x4 = 276, 356
+# manual determination of pixel coords
+# possible solution for automation would be thresholding
+x1, y1 = 206, 206
+x2, y2 = 306, 206
+x3, y3 = 206, 306
+x4, y4 = 306, 306
 
-crniKvadrati(fshift, x1, y1)
-crniKvadrati(fshift, x2, y2)
-crniKvadrati(fshift, x3, y3)
-crniKvadrati(fshift, x4, y4)
+anullPixels(fshift, x1, y1)
+anullPixels(fshift, x2, y2)
+anullPixels(fshift, x3, y3)
+anullPixels(fshift, x4, y4)
 
 # Compute the magnitude spectrum (amplitude)
 magnitude_spectrum = 20 * np.log(np.abs(fshift))
 
 # Display the magnitude spectrum
 plt.imshow(magnitude_spectrum, cmap='gray')
-plt.title('Magnituda spektra nakon uklanjanja šuma')
+plt.title('Magnitude spectrum after filtering')
 plt.savefig('fft_mag_filtered.png')
 plt.show()
-
 
 # Shift the zero-frequency component back to the top-left corner
 f_ishift = np.fft.ifftshift(fshift)
@@ -57,8 +55,7 @@ f_ishift = np.fft.ifftshift(fshift)
 img_filtered = np.fft.ifft2(f_ishift).real
 
 # Display the filtered image
-
-cv2.imshow('Final image', img_filtered.astype(np.uint8))
-cv2.imwrite('output.png', img_filtered)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv.imshow('Final image', img_filtered.astype(np.uint8))
+cv.imwrite('output.png', img_filtered)
+cv.waitKey(0)
+cv.destroyAllWindows()
